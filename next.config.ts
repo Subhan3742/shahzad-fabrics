98e1a9871f29225c42091075849d21df3e0c8f91
Subhan Nadeem <subhannadeem@saads-MacBook-Air.local>
2025-12-27 13:04:37 +0500

    next config update

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'standalone', 
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.shahzadfabricsbrandsshop.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
